cab3ea0563de64f737fd98c121294350802d376a
Shane Bauer <shane@shanebauer.com>
2015-08-11 00:23:34 -0400

    Added a few additional fields. Email and username are now different.

--- Source/MomoConnect.WebUI/Models/AccountViewModels.cs
+++ Source/MomoConnect.WebUI/Models/AccountViewModels.cs
@@ -79,6 +79,18 @@ namespace MomoConnect.WebUI.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Display Name")]
+        public string Username { get; set; }
     }
 
     public class ResetPasswordViewModel
